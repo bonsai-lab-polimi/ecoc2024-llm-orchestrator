1d055f55b4610fc90031aae007df0989dfca30c2
Nicola Di Cicco <93935338+nicoladicicco@users.noreply.github.com>
2024-03-02 17:50:53 -0500

    Adjust system prompt
    The previous version of the system prompt would fail on Q1.
    This new system  prompt allows a correct generation of Q1 that can be parsed by the verifier.

--- sample_test.py
+++ sample_test.py
@@ -5,9 +5,30 @@ from llm_orchestrator.llm_interface import LLMInterface
 
 
 def main():
-    model_path = os.path.join(".", "llama.cpp", "models", "mistral-instruct-7b", "mistral-7b-instruct-v0.2.Q5_K_M.gguf")
-    interface = LLMInterface(model_path)
-    prompt = "[INST] Generate an example .json file. The JSON file must be wrapped in ```json [JSON file here] ```[/INST]"
+    model_path = os.path.join(".", "data", "models", "mistral-7b-instruct-v0.2.Q5_K_M.gguf")
+    system_prompt_path = os.path.join(".", "data", "test_set", "system_prompt.txt")
+    question_path = os.path.join(".", "data", "test_set", "prompts", "prompt_1.txt")
+    answer_path = os.path.join(".", "data", "test_set", "ground_truths", "answer_1.json")
+
+    interface = LLMInterface(model_path, n_ctx=8192, stream=True)
+    prompt = "[INST] "
+    context = ""
+
+    # Read system prompt
+    system_prompt = open(system_prompt_path).read()
+    prompt += system_prompt
+    # Read the question
+    prompt += open(question_path).read()
+    # Read the context
+    schema_folder = os.path.join(".", "data", "json_schemas")
+    for file in os.listdir(schema_folder):
+        with open(os.path.join(schema_folder, file)) as f:
+            context += f"Schema file for {file}:\n"
+            context += f.read()
+    prompt += context
+    # Close prompt
+    prompt += "[/INST]"
+
     output = interface.generate(prompt)
     if not isinstance(output, Iterator):
         print(output["choices"][0]["text"])
