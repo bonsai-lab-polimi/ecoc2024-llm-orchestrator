9d44778924106ba5bc6dd19af0a1f909d2f0b31d
Mëmëdhe Ibrahimi <85623585+MemedheIbrahimi@users.noreply.github.com>
2024-03-07 00:20:03 +0100

    adding prompt examples and PM campaign py files

---
+++ data/test_set/ground_truths/answer_20.py
@@ -0,0 +1,32 @@
+#Performance Monitoring Campaigns Creation Parameters
+
+#Device IP address
+device = '10.79.23.39:8443'
+
+#Location of the authentication file
+path='/home/SMO/SMO-Pipeline-offline-testbed/SMO-Pipeline-offline-testbed/'
+
+#PM Campaign Name
+params = {
+        'name': 'pm_service-Node1-Node2',
+    }
+
+#Configuration Details
+json_data = {
+    #the configuration state for the PM (Performance Monitoring) session is always set to "started"
+    "configurationState": "started",
+
+    #the PM can be either with 15 minute granularity (cd15m) or a 24 hour granularity (cd24h)
+    #the possible values for the "pm" are "disabled", "enabledCurrent", and "enabledCurrentAndArchive"
+    "pm": {
+        "cd15m": "enabledOnlyCurrent",
+        "cd24h": "enabledOnlyCurrent"
+    },
+    #the "transports" field should specify the connection for which the peformance monitoring should be initiated
+    #the "transports" name field should match the field of a lightpath and/or a service
+    "transports": [
+    {
+        "name" : "service-Node1-Node2",
+    }
+    ]
+}
