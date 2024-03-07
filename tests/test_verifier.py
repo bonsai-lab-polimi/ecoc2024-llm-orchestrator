a4f8a7fc0292d7a5345b960f634e0999ea282ecd
Nicola Di Cicco <93935338+nicoladicicco@users.noreply.github.com>
2024-03-07 18:53:26 -0500

    update tests

--- tests/test_verifier.py
+++ tests/test_verifier.py
@@ -5,7 +5,7 @@ from llm_orchestrator.verifier import Verifier
 
 def test_verifier():
     verifier = Verifier(schema_dir="data/json_schemas")
-    res, _ = verifier.verify('{"name": "John"}')
+    res, _ = verifier.verify({"name": "John"})
     assert not res
 
 
