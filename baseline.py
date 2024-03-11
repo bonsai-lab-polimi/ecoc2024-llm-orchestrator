57acc4739153800f3eeb6ab36dadf812534c6214
Nicola Di Cicco <93935338+nicoladicicco@users.noreply.github.com>
2024-03-11 13:20:56 -0400

    add baseline

--- baseline.py
+++ baseline.py
@@ -1,12 +1,14 @@
 import os
 from typing import Iterator
 
+from tqdm import tqdm
+
 from llm_orchestrator.llm_interface import LLMInterface
 
 
 def main():
-    model_path = os.path.join(".", "data", "models", "mistral-7b-instruct-v0.2.Q5_K_M.gguf")
-    system_prompt_path = os.path.join(".", "data", "test_set", "system_prompt_baseline.txt")
+    model_path = os.path.join(".", "data", "models", "mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf")
+    system_prompt_path = os.path.join(".", "data", "system_prompts", "system_prompt_baseline.txt")
     questions_folder = os.path.join(".", "data", "test_set", "prompts")
     prediction_folder = os.path.join(".", "data", "test_set", "predictions_baseline")
     schema_folder = os.path.join(".", "data", "json_schemas")
@@ -18,14 +20,19 @@ def main():
         "Measurement": "measurement_schema.json",
         "Service": "service_schema.json",
     }
-    task_to_schema = {task: open(os.path.join(schema_folder, schema)).read() for task, schema in task_to_schema_path.items()}
+    task_to_schema = {
+        task: open(os.path.join(schema_folder, schema)).read() for task, schema in task_to_schema_path.items()
+    }
 
     # Read system prompt
     system_prompt = open(system_prompt_path).read()
 
-    for question_file in os.listdir(questions_folder):
+    for question_file in tqdm(os.listdir(questions_folder)):
         question_path = os.path.join(questions_folder, question_file)
         question_id = question_file.split("_")[1].split(".")[0]
+        # If prediction already exists, skip
+        if os.path.exists(os.path.join(prediction_folder, f"prediction_{question_id}.txt")):
+            continue
         # Read the question
         question = "User: " + open(question_path).read()
         # Concatenate all the json schemas in one big string:
@@ -34,7 +41,7 @@ def main():
             context = context + f"Schema for {task}:" + schema + "\n"
         # Generate the tokens for the full prompt
         # Mistral Instruct requires two special tokens to start and end the prompt
-        tokens = interface.tokenize("[INST]" + system_prompt + question + "Assistant:\n" "[/INST]")
+        tokens = interface.tokenize("[INST]" + system_prompt + question + context + "Assistant:\n" "[/INST]")
         output = interface.generate(tokens, max_tokens=0, seed=42)
         if not isinstance(output, Iterator):
             answer = output["choices"][0]["text"]
